93f1185be60674bdb6c5197739264bf94c60cb46
DucToan <96660892+toanuitt@users.noreply.github.com>
2025-03-31 17:53:49 +0700

    chore: change structure task.go

--- backend/models/task.go
+++ backend/models/task.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Task struct {
-	ID 			int 				`json:"id"`
+	ID 		int 				`json:"id"`
 	Title 		string				`json:"title"`
-	Description string				`json:"description"`
-	Priority 	enums.TaskPriority  `json:"priority"`
-	Category 	enums.TaskCategory  `json:"category"`
+	Description 	string				`json:"description"`
+	Priority 	enums.TaskPriority  		`json:"priority"`
+	Category 	enums.TaskCategory  		`json:"category"`
 	DueDate 	time.Time			`json:"dueDate"`
 	Completed 	bool				`json:"completed"`
 }
